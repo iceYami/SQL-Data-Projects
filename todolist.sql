b1a057b86da2deca72891d404521d624baf62d41
iceYami <137309137+iceYami@users.noreply.github.com>
2024-10-20 10:23:49 +0200

    Create todolist.sql

---
+++ todolist.sql
@@ -0,0 +1,13 @@
+CREATE TABLE todo_list (
+    id INTEGER PRIMARY KEY,
+    item TEXT,
+    minutes INTEGER
+);
+
+INSERT INTO todo_list (item, minutes) VALUES ('Estudiar Ciberseguridad', 60);
+INSERT INTO todo_list (item, minutes) VALUES ('Implementar la base de datos del Ayuntamiento', 30);
+INSERT INTO todo_list (item, minutes) VALUES ('Hacer dominadas', 15);
+INSERT INTO todo_list (item, minutes) VALUES ('Limpiar la casa', 20);
+INSERT INTO todo_list (item, minutes) VALUES ('Leer un libro', 30);
+INSERT INTO todo_list (item, minutes) VALUES ('Preparar la comida', 30);
+INSERT INTO todo_list (item, minutes) VALUES ('Hacer los deberes del curso', 30);
